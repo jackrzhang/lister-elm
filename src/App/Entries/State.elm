85864598c1c31751e12905205e0e4f060d14b005
Jack Zhang <15jzhang@gmail.com>
2017-01-06 21:49:12 -0500

    Update Entries currentId on AddEntry

--- src/App/Entries/State.elm
+++ src/App/Entries/State.elm
@@ -8,10 +8,7 @@ import App.Control.Types exposing (Filter(..))
 
 initialModel : Model
 initialModel =
-    { list =
-        [ Entry -1 "First Entry" False
-        , Entry -2 "Second Entry" True
-        ]
+    { list = []
     , filter = All
     , currentId = 0
     }
@@ -29,13 +26,16 @@ updateModel msg model =
     case msg of
 
         AddEntry text ->
-            { model | list = List.append model.list [ Entry model.currentId text False ] }
+            { model 
+                | list = List.append model.list [ Entry model.currentId text False ]
+                , currentId = model.currentId + 1
+            }
 
         RemoveEntry id ->
             { model | list = List.filter (\entry -> not <| entry.id == id) model.list }
 
         ToggleComplete id ->
-                    { model | list = toggleComplete id model.list }
+            { model | list = toggleComplete id model.list }
 
         ApplyFilter filter ->
             { model | filter = filter }
